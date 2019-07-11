a7198ff588086522dd7a255b791aad6f592b956a
ldc4 <ldc4@vip.qq.com>
2019-07-11 16:03:33 +0800

    添加简单静态页面webpack脚手架

---
+++ static/config/webpack.common.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, '../dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(less|css)$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'postcss-loader' },
+          { loader: 'less-loader' }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 8192 }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: { bypassOnDebug: true }
+          }
+        ]
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            name: 'static/[name].[ext]?[hash:6]'
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'head',
+      minify: {
+        collapseWhitespace: true
+      }
+    })
+  ]
+};
